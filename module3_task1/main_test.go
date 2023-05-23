534a0ced3bc4c436a3eff3e1383e9a5f61ef3424
sonia <https://github.com/Soniabensaad>
2023-05-23 14:27:43 +0200

    hello go

--- module3_task1/main_test.go
+++ module3_task1/main_test.go
@@ -31,24 +31,18 @@ func Test_HelloHandler(t *testing.T) {
       body:         "Hello Rosalind Franklin!",
     },
     // INSERT MORE TESTS HERE
-    {
-      name:         "Empty name",
-      queryString:  "name=",
-      responseCode: 400,
-      body:         "",
-    },
-    {
-      name:         "No name",
-      queryString:  "",
-      responseCode: 200,
-      body:         "Hello there!",
-    },
-    {
-      name:         "Multiple names",
-      queryString:  "name=Foo&Name=Bar",
-      responseCode: 200,
-      body:         "Hello Foo!",
-    },
+	{
+		name: "Ethan Benya",
+		queryString: "",
+		responseCode: 200,
+		body: "Hello there!",
+	},
+	{
+		name: "Baptiste Baptman",
+		queryString: "name=",
+		responseCode: 400,
+		body: "",
+	},
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
